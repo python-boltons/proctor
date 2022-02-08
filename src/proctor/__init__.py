006bef04cac713ce877d46a557f2914fca9857e0
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-02-08 16:58:17 -0500

    Bump version: 0.2.0 → 0.2.1

--- src/proctor/__init__.py
+++ src/proctor/__init__.py
@@ -12,6 +12,6 @@ __all__ = ["Process", "command_exists", "safe_popen", "unsafe_popen"]
 
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 _logging.getLogger(__name__).addHandler(_logging.NullHandler())
